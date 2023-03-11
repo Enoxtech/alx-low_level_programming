3434eb5eb39f8e8c25fdb7c359c5a0f9ae8a519b
Enoxx <skybondnyc@gmail.com>
2023-03-10 17:21:11 -0800

    Updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,22 +8,23 @@
  *
  * Return: Always 0 (Success)
  */
-
 int check_num(char *str)
 {
 	/*Declaring variables*/
 	unsigned int count;
+
 	count = 0;
 	while (count < strlen(str)) /*count string*/
-{
-	if (!isdigit(str[count])) /*check if str there are digit*/
-{
-	return (0);
-}
 
-count++;
-}
-return (1);
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
 }
 
 /**
@@ -35,27 +36,35 @@ return (1);
  */
 
 int main(int argc, char *argv[])
+
 {
+
 	/*Declaring variables*/
 	int count;
 	int str_to_int;
 	int sum = 0;
+
 	count = 1;
 	while (count < argc) /*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
-	{
-		str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-		sum += str_to_int;
-	}
+
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
+		}
+
 		/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+
 		count++;
 	}
+
 	printf("%d\n", sum); /*print sum*/
+
 	return (0);
 }
